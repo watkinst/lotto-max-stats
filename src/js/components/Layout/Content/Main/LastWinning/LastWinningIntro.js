197651238b202cf3c3503f36ef3283806e803ec5
Trevor Watkinson <trevor.william.watkinson@gmail.com>
2016-08-11 22:36:45 -0300

    adds last winning numbers component

---
+++ src/js/components/Layout/Content/Main/LastWinning/LastWinningIntro.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+require('../../../../../../sass/lastwinningintro.scss');
+
+export default class LastWinningIntro extends React.Component {
+
+  getLastDrawDate() {
+    var lastDraw = this.props.getLastDraw();
+    return lastDraw.date;
+  }
+
+  render() {
+
+    var lastDrawDate = this.getLastDrawDate();
+
+    return (
+      <div className="col-xs-12 last-winning-intro">
+        <h2 className="title">Last Winning Numbers</h2>
+        <p>Here are the winning numbers from the draw held on {lastDrawDate} (bonus in green).</p>
+      </div>
+    );
+  }
+}
---
+++ src/js/components/Layout/Content/Main/LastWinning/LastWinningNumbers.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+require('../../../../../../sass/lastwinningnumbers.scss');
+
+export default class LastWinningNumbers extends React.Component {
+  
+  getLastBonusNumber() {
+    var lastDraw = this.props.getLastDraw();
+    return lastDraw.lotto_max.bonus_number;
+  }
+
+  getLastWinningNumbers() {    
+    var lastDraw = this.props.getLastDraw();
+    return lastDraw.lotto_max.winning_numbers;
+  }
+
+  render() {
+
+    var lastWinningNumbers = this.getLastWinningNumbers();
+    var lastBonusNumber = this.getLastBonusNumber();
+
+    return (
+      <div className="col-xs-12 last-winning-numbers">
+        <button className="btn btn-default btn-last-winning disabled">{lastWinningNumbers[0]}</button>
+        <button className="btn btn-default btn-last-winning disabled">{lastWinningNumbers[1]}</button>
+        <button className="btn btn-default btn-last-winning disabled">{lastWinningNumbers[2]}</button>
+        <button className="btn btn-default btn-last-winning disabled">{lastWinningNumbers[3]}</button>
+        <button className="btn btn-default btn-last-winning disabled">{lastWinningNumbers[4]}</button>
+        <button className="btn btn-default btn-last-winning disabled">{lastWinningNumbers[5]}</button>
+        <button className="btn btn-default btn-last-winning disabled">{lastWinningNumbers[6]}</button>
+        <button className="btn btn-success btn-last-winning disabled">{lastBonusNumber}</button>
+      </div>
+    );
+  }
+}
