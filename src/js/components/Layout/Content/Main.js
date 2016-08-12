a8f8639643809bd7ce84e3389d4a150bb1a64710
Trevor Watkinson <trevor.william.watkinson@gmail.com>
2016-08-12 00:01:27 -0300

    adds most frequently drawn bonus number to most frequent numbers component

--- src/js/components/Layout/Content/Main.js
+++ src/js/components/Layout/Content/Main.js
@@ -5,24 +5,6 @@ import MostFrequent from './Main/MostFrequent';
 
 export default class Main extends React.Component {
 
-  calculateTimesDrawn() {
-    var timesDrawn = [];
-
-    for (var i = 0; i < 49; i++) {
-      timesDrawn.push(0);
-    }
-    
-    var draws = this.props.data.draws;
-
-    for (var i = 0; i < draws.length; i++) {
-      for (var j = 0; j < draws[i].lotto_max.winning_numbers.length; j++) {
-        timesDrawn[draws[i].lotto_max.winning_numbers[j]-1]++;
-      }
-    }    
-
-    return timesDrawn;
-  }
-
   render() {
     return (
       <div className="col-xs-12 col-lg-9">
@@ -31,7 +13,7 @@ export default class Main extends React.Component {
           <div className="col-xs-12 col-md-6">
             <div className="row">
               <LastWinning data={this.props.data} />
-              <MostFrequent calculateTimesDrawn={this.calculateTimesDrawn.bind(this)} />
+              <MostFrequent data={this.props.data} />
             </div>
           </div>
         </div>
