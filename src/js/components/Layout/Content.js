ddd9dc8a0b48632ef5dddccec99de6810fa573f9
Trevor Watkinson <trevor.william.watkinson@gmail.com>
2016-08-20 13:52:30 -0300

    adds 15px bottom margin to content

--- src/js/components/Layout/Content.js
+++ src/js/components/Layout/Content.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Main from './Content/Main';
 import Sidebar from './Content/Sidebar';
 
+require('../../../sass/content.scss');
+
 export default class Content extends React.Component {
   render() {
     return (
       <div className="row">
         <div className="col-xs-12">
-          <div className="row">
-            <Main data={this.props.data} />
-            <Sidebar />
+          <div className="content">
+            <div className="row">
+              <Main data={this.props.data} />
+              <Sidebar />
+            </div>
           </div>
         </div>
       </div>
