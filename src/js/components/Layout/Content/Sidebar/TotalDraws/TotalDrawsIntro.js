cca29a08d25c70837e7cee3148dfe6afe389cf5a
Trevor Watkinson <trevor.william.watkinson@gmail.com>
2016-08-22 23:43:58 -0300

    fixes a small spelling mistake

--- src/js/components/Layout/Content/Sidebar/TotalDraws/TotalDrawsIntro.js
+++ src/js/components/Layout/Content/Sidebar/TotalDraws/TotalDrawsIntro.js
@@ -8,7 +8,7 @@ export default class TotalDrawsIntro extends React.Component {
     return (
       <div className="col-xs-12 total-draws-intro">
         <h2 className="title">Total Lotto Max Draws</h2>
-        <p>Here is the total number of Lotto Max Draws so far.</p>
+        <p>Here is the total number of Lotto Max draws so far.</p>
       </div>
     );
   }
